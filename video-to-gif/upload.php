064a928ce6896b229279434573b7f24921ccaa67
Sebastian Sperandio <C-SSperandio@nexstar.tv>
2025-04-30 18:54:13 -0300

    Add debug code

--- video-to-gif/upload.php
+++ video-to-gif/upload.php
@@ -15,18 +15,17 @@ if (!file_exists("outputs")) {
 }
 
 // Build FFmpeg command
-/* $cmd = "ffmpeg -i {$inputVideo} -vf \
-  "select='gt(scene,0.4)',scale=480:-1,fps=12" \
-  -t 3 -y {$outputGif}"; */
 $ffmpeg = '/home/sdbgdes/ffmpeg-git-20180203-amd64-static/ffmpeg';
 $cmd = "$ffmpeg -i {$inputVideo} -vf \"select='gt(scene,0.4)',scale=480:-1,fps=12\" -t 3 -y {$outputGif}";
   
-exec($cmd, $output, $return_var);
+exec($cmd . " 2>&1", $output, $return_var);
 
 if ($return_var === 0 && file_exists($outputGif)) {
     echo "<h2>✅ GIF Created!</h2><img src='{$outputGif}' alt='Generated GIF' class='generated-gif'><br>";
     echo "<a href='{$outputGif}' download class='button-main'>Download GIF</a>";
 } else {
     echo "<p class='error-message'>❌ Error generating GIF.</p>";
+    echo "<pre><code>" . htmlspecialchars(implode("\n", $output)) . "</code></pre>";
 }
+
 ?>
